6f881f3ce9c30671fb33a6297d608d21ab53b2c8
mario agouloye <agouloyemario@outlook.fr>
2025-01-05 22:00:36 +0100

    refactor: deactivate auth checking

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@
 import { Toaster } from "@/components/ui/toaster";
 // import { NavigationProvider } from "@/components/providers/navigation-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "@/lib/auth/auth-provider";
+// import { AuthProvider } from "@/lib/auth/auth-provider";
 import { useEffect } from "react";
 import { getCsrfToken } from "@/lib/api/auth";
 
@@ -29,10 +29,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
+      {/*<AuthProvider>*/}
         <>{children}</>
         <Toaster />
-      </AuthProvider>
+      {/*</AuthProvider>*/}
     </QueryClientProvider>
   );
 }
