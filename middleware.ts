c2f133b30a30b171a30c6e08c9e0913d485a2e0e
Mario Agouloye <agouloyemario@outlook.fr>
2025-01-07 01:34:06 +0100

    refactor: middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   // const token = request.cookies.get("token")?.value;
-  const user = request.cookies.get("user")?.value;
+  const user = localStorage.getItem("user");
   const isAuthPage =
     request.nextUrl.pathname.startsWith("/login") ||
     request.nextUrl.pathname.startsWith("/register");
